bb7503a7584f8d57b3ad89eb92cdee3881636250
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-07-09 22:37:31 +0530

    Fibonacci - Space complexity O(1) approach

--- Dynamic Programming/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci.cpp
@@ -30,6 +30,23 @@ int fib_bot_up(int n){
   return dp[n];
 }
 
+// space complexity O(1) approach
+int fib_optimized(int n){
+  if(n==0 || n==1) {
+    return n;
+  }
+
+  int a = 0, b = 1, c;
+
+  for(int i=2; i<=n; i++){
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
+}
+
 int main() {
   int n;
   cin>>n;
