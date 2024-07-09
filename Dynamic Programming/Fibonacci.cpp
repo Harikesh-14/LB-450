4aafe0cbd5c89a514e5cc5f78c01a3c24daa5fc6
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-07-09 15:26:17 +0530

    Fibonacci using DP

---
+++ Dynamic Programming/Fibonacci.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fib(int n, vector<int> &dp){
+  if(n==0 || n==1) {
+    return n;
+  }
+
+  if(dp[n] != -1) {
+    return dp[n];
+  }
+
+  dp[n] = fib(n-1, dp) + fib(n-2, dp);
+
+  return dp[n];
+}
+
+int main() {
+  int n;
+  cin>>n;
+
+  vector<int> dp(n+1, -1);
+
+  cout<<fib(n, dp)<<endl;
+
+  return 0;
+}
