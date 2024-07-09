4f776b842802130810d6282fd20c9db7ebcbebae
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-07-09 15:39:36 +0530

    Fibonacci - Bottom Up Approach (DP)

--- Dynamic Programming/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 
+// Top Down Approach
 int fib(int n, vector<int> &dp){
   if(n==0 || n==1) {
     return n;
@@ -16,13 +17,29 @@ int fib(int n, vector<int> &dp){
   return dp[n];
 }
 
+// Bottom Up Approach
+int fib_bot_up(int n){
+  vector<int> dp(n+1);
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for(int i=2; i<=n; i++){
+    dp[i] = dp[i-1] + dp[i-2];
+  }
+
+  return dp[n];
+}
+
 int main() {
   int n;
   cin>>n;
 
   vector<int> dp(n+1, -1);
 
+  cout<<"Top Down Approach: ";
   cout<<fib(n, dp)<<endl;
 
+  cout<<"Bottom Up Approach: ";
+  cout<<fib_bot_up(n)<<endl;
   return 0;
 }
