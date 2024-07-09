bf63d908e4f85edf7fae7fcaf2e09c01305656c0
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-07-09 21:39:56 +0530

    Ways to count stair using DP (Bottom Up approach)

--- Dynamic Programming/ways_to_reach_nth_stair.cpp
+++ Dynamic Programming/ways_to_reach_nth_stair.cpp
@@ -22,6 +22,20 @@ int countDistinctWays(int stairs, int i, vector<int> &dp){
   return dp[i] = (countDistinctWays(stairs, i+1, dp) + countDistinctWays(stairs, i+2, dp)) % MOD;
 }
 
+// Bottom up approach
+int countWays(int stairs){
+  vector<int> dp(stairs + 1);
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for(int i=2; i<=stairs; i++){
+    dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+  }
+
+  return dp[stairs];
+}
+
 int main() {
   int n;
   cin>>n;
@@ -31,6 +45,9 @@ int main() {
   cout<<"Top down approach: ";
   int ans = countDistinctWays(n, 0, dp);
   cout<<ans<<endl;
+
+  cout<<"Bottom up approach: ";
+  cout<<countWays(n)<<endl;
 
   return 0;
 }
